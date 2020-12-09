40858e69e0e0a3d89f774e9ab101f80ba939cd01
MegSemrad <mlsemrad753@gmail.com>
2020-12-08 21:14:11 -0600

    add boilerplate code

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,12 +1,20 @@
 const contentTarget = document.querySelector(".entry")
 const eventHub = document.querySelector("#state-fair")
 
-eventHub.addEventListener()
+eventHub.addEventListener("click", clickEvent => {
+    if(clickEvent.target.id === "rideTicket") {
+       const rideEvent = new CustomEvent("rideTicketPurchsed", {
+           detail: {
+
+           }
+       }) 
+    }
+}
 
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
-            Add button here
+            <button id="rideTicket">Ride Ticket</button>
         </div>
     `
 }
