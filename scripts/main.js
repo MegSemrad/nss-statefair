cdf42e126068f7bcc11b32c3e33d4adcbdedb964
MegSemrad <mlsemrad753@gmail.com>
2020-12-09 13:29:21 -0600

    finished game ticket section

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 import { TicketBooth } from './TicketBooth.js';
 import { RideTicketHolders } from './rides/RideTicketHolders.js';
 import { FoodTicketHolders } from './food/FoodTicketHolder.js';
+import { GameTicketHolders } from './games/GameTicketHolder.js';
 
 
 TicketBooth();
 RideTicketHolders();
 FoodTicketHolders();
+GameTicketHolders();
 
 /* Ask why RideTicketHolders needs to be imported if we don't 
 want it to populate the page when loads
