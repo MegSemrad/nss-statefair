c109c3812aa3df2cef8c2e0885a8b84cbe171418
MegSemrad <mlsemrad753@gmail.com>
2020-12-18 14:04:12 -0600

    rewrote code to be more dynamic

--- scripts/fullPackage/fullPackageTicketHolders.js
+++ scripts/fullPackage/fullPackageTicketHolders.js
@@ -2,19 +2,19 @@ const eventHub = document.querySelector("#state-fair");
 const contentTarget = document.querySelectorAll(".people");
 
 
+
 export const fullPackageTicketHolders = () => {
         eventHub.addEventListener("fullPackageTicketPurchased", event => {
-            contentTarget[0].innerHTML += `
+            let i;
+            for(i = 0; i < contentTarget.length; i++) {
+                contentTarget[i].innerHTML += `
                 <div class="bigSpender person"></div>
             `
-            contentTarget[1].innerHTML += `
-                <div class="bigSpender person"></div>
-            `
-            contentTarget[2].innerHTML += `
-                <div class="bigSpender person"></div>
-            `
-            contentTarget[3].innerHTML += `
-                <div class="bigSpender person"></div>
-            `
+            }
         })
-    };
+}
+ 
+
+    /*
+    - .querySelectorAll() is a method 
+    */
